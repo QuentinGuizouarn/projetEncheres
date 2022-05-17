f7ceb9e8d6e3cf130ee4cc68f0a754b76cca2a88
QuentinGuizouarn <quentin.guizouarn2021@campus-eni.fr>
2022-05-17 14:26:45 +0200

    Ajout Interface UtilisateurDAO

---
+++ src/main/java/dal/UtilisateurDAO.java
@@ -0,0 +1,15 @@
+package dal;
+
+import java.sql.SQLException;
+
+import bo.Utilisateur;
+
+public interface UtilisateurDAO {
+	
+	public void insert(Utilisateur u) throws SQLException;
+	public Utilisateur selectById(int id) throws SQLException;
+	public Utilisateur selectByConnection(String pseudo, String motDePasse) throws SQLException;
+	public void update(Utilisateur u) throws SQLException;
+	public void delete(int id) throws SQLException;
+
+}
