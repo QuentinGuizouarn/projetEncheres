ba26fd816ebb9b1e5a27a2b38775553b49c08d9b
QuentinGuizouarn <quentin.guizouarn2021@campus-eni.fr>
2022-05-17 15:49:45 +0200

    Update EnchereDAO : retirer méthode delete (pas besoin)

--- src/main/java/dal/iDAO/EnchereDAO.java
+++ src/main/java/dal/iDAO/EnchereDAO.java
@@ -8,6 +8,5 @@ public interface EnchereDAO {
 
 	public void insert(Enchere e) throws SQLException;
 	public Enchere selectByArticle(int id) throws SQLException;
-	public void delete(int id) throws SQLException;
 	
 }
