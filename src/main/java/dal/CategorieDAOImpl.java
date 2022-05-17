7fcea8e85d6661b284304e7ffc54def54dc204fe
QuentinGuizouarn <quentin.guizouarn2021@campus-eni.fr>
2022-05-17 14:58:49 +0200

    Ajout classe CategorieDAOImpl + modif retour selectAll

---
+++ src/main/java/dal/CategorieDAOImpl.java
@@ -0,0 +1,41 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import bo.Categorie;
+
+public class CategorieDAOImpl implements CategorieDAO {
+	
+	private static final String INSERT = "INSERT INTO CATEGORIES (libelle) VALUES (?)";	
+	private static final String SELECT_ALL = "SELECT * FROM CATEGORIES";
+	
+	public Connection cnx;
+	public CategorieDAOImpl() throws SQLException {
+		cnx = ConnectionProvider.getConnection();
+	}
+
+	@Override
+	public void insert(Categorie c) throws SQLException {
+		PreparedStatement ps = cnx.prepareStatement(INSERT);
+		ps.setString(1, c.getLibelle());
+		ps.executeUpdate();
+		cnx.close();	
+	}
+
+	@Override
+	public List<Categorie> selectAll() throws SQLException {
+		List<Categorie> liste = null;
+		PreparedStatement ps = cnx.prepareStatement(SELECT_ALL);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			liste.add(new Categorie( rs.getString(1) ));
+		}
+		cnx.close();
+		return liste;
+	}
+
+}
