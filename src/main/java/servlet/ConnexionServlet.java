9972b7e19982d3686382770171ff29c72705d809
AgatheVernon <105640249+AgatheVernon@users.noreply.github.com>
2022-05-18 16:19:40 +0200

    Modification servlet connexion

--- src/main/java/servlet/ConnexionServlet.java
+++ src/main/java/servlet/ConnexionServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,28 +11,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import helpers.HashPassword;
+
 /**
- * Servlet implementation class listeEnchereServlet
+ * Servlet implementation class ConnexionServlet
  */
-@WebServlet("/liste")
-public class listeEnchereServlet extends HttpServlet {
+@WebServlet("/connexion")
+public class ConnexionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	
-		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/listeEnchere.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/connexion.jsp");
 		rd.forward(request, response);
-		
-		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
+		String email = request.getParameter("email");
+		String motDePass = HashPassword.hashpassword(request.getParameter("motdepasse"));
+		
+		if()
+	}	
 }
