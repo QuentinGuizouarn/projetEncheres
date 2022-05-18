61a284d5f9a96413840ca1483ee279abff337b12
QuentinGuizouarn <quentin.guizouarn2021@campus-eni.fr>
2022-05-18 09:27:15 +0200

    Update EnchereDAO (dal) : modification paramètre méthode selectByArticle

--- src/main/java/dal/iDAO/EnchereDAO.java
+++ src/main/java/dal/iDAO/EnchereDAO.java
@@ -2,11 +2,12 @@ package dal.iDAO;
 
 import java.sql.SQLException;
 
+import bo.ArticleVendu;
 import bo.Enchere;
 
 public interface EnchereDAO {
 
 	public void insert(Enchere e) throws SQLException;
-	public Enchere selectByArticle(int idArticle) throws SQLException;
+	public Enchere selectByArticle(ArticleVendu av) throws SQLException;
 	
 }
