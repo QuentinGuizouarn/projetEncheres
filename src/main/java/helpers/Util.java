6ecdbe6c841ac96c3415d46a40c5f429ae981a6b
QuentinGuizouarn <quentin.guizouarn2021@campus-eni.fr>
2022-05-18 11:07:35 +0200

    Create Util (helpers) : hashpassword

---
+++ src/main/java/helpers/Util.java
@@ -0,0 +1,33 @@
+package helpers;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Util {
+	
+	public static String hashpassword(String password) {
+		String generatedPassword = null;
+	    try 
+	    {
+	      MessageDigest md = MessageDigest.getInstance("MD5");
+	      md.update(password.getBytes());
+	      byte[] bytes = md.digest();
+
+	      StringBuilder sb = new StringBuilder();
+	      for (int i = 0; i < bytes.length; i++) {
+	        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	      }
+	      generatedPassword = sb.toString();
+	    } catch (NoSuchAlgorithmException e) {
+	      e.printStackTrace();
+	    }	
+	    return generatedPassword;
+	}
+	
+	public static void checkField(String message, String s) throws Exception {
+		if (s.isEmpty() || s.isBlank()) {
+			throw new Exception(message);
+		}
+	}
+
+}
