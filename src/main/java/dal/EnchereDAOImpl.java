01603b70fc47cce1490eae52cf5f5a6a776108ed
QuentinGuizouarn <quentin.guizouarn2021@campus-eni.fr>
2022-05-20 11:25:55 +0200

    Correction requête (inversion entre le vendeur et l'acheteur)

--- src/main/java/dal/EnchereDAOImpl.java
+++ src/main/java/dal/EnchereDAOImpl.java
@@ -22,14 +22,13 @@ public class EnchereDAOImpl implements EnchereDAO {
 			+ "	A.codePostal, A.ville, A.etat, idAcheteur = UA.idUtilisateur, pseudoAcheteur = UA.pseudo,"
 			+ "	idVendeur = UV.idUtilisateur, pseudoVendeur = UV.pseudo, telephoneVendeur = UV.telephone"
 			+ "	FROM ENCHERES E"
-			+ "	INNER JOIN UTILISATEURS UV ON UV.idUtilisateur = E.idUtilisateur"
+			+ "	INNER JOIN UTILISATEURS UA ON UA.idUtilisateur = E.idUtilisateur"
 			+ "	INNER JOIN ARTICLES_VENDUS A ON A.idArticle = E.idArticle"
-			+ "	INNER JOIN UTILISATEURS UA ON UA.idUtilisateur = A.idUtilisateur"
+			+ "	INNER JOIN UTILISATEURS UV ON UV.idUtilisateur = A.idUtilisateur"
 			+ "	INNER JOIN CATEGORIES C ON C.idCategorie = A.idCategorie"
 			+ "	WHERE E.idArticle = ?"
-			+ "	GROUP BY E.date, E.montant, A.idArticle, A.nom, A.description, A.idCategorie, C.libelle, A.prixInitial,"
-			+ "	A.prixVente, A.dateDebut, A.dateFin, A.rue, A.codePostal, A.ville, A.etat, UA.idUtilisateur, UA.pseudo,"
-			+ "	UV.idUtilisateur, UV.pseudo, UV.telephone";
+			+ "	GROUP BY E.date, E.montant, A.idArticle, A.nom, A.description, A.idCategorie, C.libelle, A.prixInitial, A.prixVente,"
+			+ "	A.dateDebut, A.dateFin, A.rue, A.codePostal, A.ville, A.etat, UA.idUtilisateur, UA.pseudo, UV.idUtilisateur, UV.pseudo, UV.telephone";
 	
 	public Connection cnx;
 	public EnchereDAOImpl() throws SQLException {
